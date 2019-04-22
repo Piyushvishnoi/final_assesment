1180cecd32a38722f01a53267d5a8850e9c361c9
piyush vishnoi <piyushvishnoipvsh123@gmail.com>
2019-04-23 04:50:22 +0530

    created a structure

---
+++ file.cpp
@@ -0,0 +1,16 @@
+/*
+ * C++ Program to Implement Singly Linked List
+ */
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+/*
+ * Node Declaration
+ */
+struct node
+{
+    int info;
+    struct node *next;
+}*start;
+ 
